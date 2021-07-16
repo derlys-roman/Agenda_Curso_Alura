ee44ff7b9ad0fa15384307973751bf20b2ed21eb
Derlys <derlysr6@gmail.com>
2021-07-16 00:55:45 -0300

    Adicionado View ListView no activity_main.xml e preenchido via adapter no MainActivity.kt, sendo que foi utilizado uma lista previamente criada no arquivo kotlin, nesta  parte foi usado um ArrayAdapter e o aprendizado foi de como sincronizar uma lista de String com uma listview

--- app/src/main/java/br/com/alura/agendadecadastrodealunodocursodaalura/MainActivity.kt
+++ app/src/main/java/br/com/alura/agendadecadastrodealunodocursodaalura/MainActivity.kt
@@ -1,11 +1,17 @@
 package br.com.alura.agendadecadastrodealunodocursodaalura
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val namesListOfStudents : MutableList<String> = mutableListOf("Johnny", "Marcus", "Johanna", "Lucas")
+        val viewListOfStudents : ListView = findViewById(R.id.main_activity_list_of_students)
+        viewListOfStudents.adapter =
+            ArrayAdapter(this, android.R.layout.simple_list_item_1, namesListOfStudents)
     }
 }
