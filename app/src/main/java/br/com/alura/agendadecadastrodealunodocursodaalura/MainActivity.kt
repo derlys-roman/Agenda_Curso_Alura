e64ba6b96d606d0dbb44e6b521891ca8dce53375
Derlys <derlysr6@gmail.com>
2021-07-19 04:22:52 -0300

    Implement ViewBinding

--- app/src/main/java/br/com/alura/agendadecadastrodealunodocursodaalura/MainActivity.kt
+++ app/src/main/java/br/com/alura/agendadecadastrodealunodocursodaalura/MainActivity.kt
@@ -4,14 +4,16 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import br.com.alura.agendadecadastrodealunodocursodaalura.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         val namesListOfStudents : MutableList<String> = mutableListOf("Johnny", "Marcus", "Johanna", "Lucas")
-        val viewListOfStudents : ListView = findViewById(R.id.main_activity_list_of_students)
-        viewListOfStudents.adapter =
+        binding.mainActivityListOfStudents.adapter =
             ArrayAdapter(this, android.R.layout.simple_list_item_1, namesListOfStudents)
     }
 }
