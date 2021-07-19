d76037b3340dec1eab764301be31796005c126d5
Derlys <derlysr6@gmail.com>
2021-07-19 06:28:30 -0300

    Add temporary fun to add a name to list

--- app/src/main/java/br/com/alura/agendadecadastrodealunodocursodaalura/MainActivity.kt
+++ app/src/main/java/br/com/alura/agendadecadastrodealunodocursodaalura/MainActivity.kt
@@ -2,18 +2,32 @@ package br.com.alura.agendadecadastrodealunodocursodaalura
 
 import android.os.Bundle
 import android.widget.ArrayAdapter
-import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import br.com.alura.agendadecadastrodealunodocursodaalura.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+
+    private val namesListOfStudents : MutableList<String> = mutableListOf("Johnny", "Marcus", "Johanna", "Lucas")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        title = "Aluno"
         setContentView(binding.root)
-        val namesListOfStudents : MutableList<String> = mutableListOf("Johnny", "Marcus", "Johanna", "Lucas")
+        listAdapting()
+
+        binding.mainActivityFloatingButton.setOnClickListener { clicnadoButton() }
+
+    }
+
+    private fun listAdapting(){
         binding.mainActivityListOfStudents.adapter =
             ArrayAdapter(this, android.R.layout.simple_list_item_1, namesListOfStudents)
+    }
+    private fun clicnadoButton(){
+        namesListOfStudents += listOf("Shepard")
+        return listAdapting()
     }
 }
