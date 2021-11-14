29bd5577396bed05bf2e3ffe6b85a2acb90c2988
Derlys <derlysr6@gmail.com>
2021-11-13 22:49:26 -0300

    Add AlertDialog to confirm delete

--- app/src/main/java/br/com/alura/agendadecadastrodealunodocursodaalura/ui/activity/StudentListActivity.kt
+++ app/src/main/java/br/com/alura/agendadecadastrodealunodocursodaalura/ui/activity/StudentListActivity.kt
@@ -1,5 +1,6 @@
 package br.com.alura.agendadecadastrodealunodocursodaalura.ui.activity
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.view.ContextMenu
@@ -7,6 +8,7 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ListView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import br.com.alura.agendadecadastrodealunodocursodaalura.R
 import br.com.alura.agendadecadastrodealunodocursodaalura.dao.PeopleDAO
@@ -14,6 +16,7 @@ import br.com.alura.agendadecadastrodealunodocursodaalura.databinding.ActivityLi
 import br.com.alura.agendadecadastrodealunodocursodaalura.model.People
 import br.com.alura.agendadecadastrodealunodocursodaalura.ui.adapter.ListAdapter
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.android.synthetic.main.list_student_presentation.*
 
 class StudentListActivity(private val dao: PeopleDAO = PeopleDAO()) :
     AppCompatActivity(), ConstantsActivities {
@@ -47,10 +50,22 @@ class StudentListActivity(private val dao: PeopleDAO = PeopleDAO()) :
         //Get item info for click, use adapter
         val menuInfo: AdapterView.AdapterContextMenuInfo =
             item.menuInfo as AdapterView.AdapterContextMenuInfo
+        val toRemove : People= adapter.getItem(menuInfo.position) as People
         if (item.itemId == R.id.activity_student_menu_remove) {
-            removeOfList(adapter.getItem(menuInfo.position) as People)
+            confirmRemove(toRemove)
         }
         return super.onContextItemSelected(item)
+    }
+
+    private fun confirmRemove(toRemove: People) {
+        AlertDialog.Builder(this)
+            .setTitle(R.string.remove_item)
+            .setMessage(R.string.ready_to_remove)
+            .setPositiveButton(R.string.yes, DialogInterface.OnClickListener { dialog, which ->
+                removeOfList(toRemove)
+            })
+            .setNegativeButton(R.string.no, null)
+            .show()
     }
 
     override fun onResume() {
